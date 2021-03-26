ae0e58a069110b115db6634877bfbcf0313fc5b3
tokku5552 <physics.0121@gmail.com>
2021-03-26 22:48:41 +0900

    feat:material-uiの導入、README修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <AppBar></AppBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
