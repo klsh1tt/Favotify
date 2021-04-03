808e6599430faae7ef265803d071274545128095
tokku5552 <physics.0121@gmail.com>
2021-04-03 18:15:34 +0900

    feat:Loginページから画面遷移できるように修正

--- src/templates/Login.tsx
+++ src/templates/Login.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useDispatch } from "react-redux";
+import { push } from "connected-react-router";
 
 const useStyles = makeStyles({
   p: {
     // margin:'auto',
     // width:'50%',
-    textAlign:'center',
+    textAlign: "center",
+  },
+  h2: {
+    // margin:'auto',
+    // width:'50%',
+    textAlign: "center",
   },
 });
 
 const Login = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <p className={classes.p}>login page</p>
+      <h2 className={classes.h2}>ログイン</h2>
+      <button onClick={() => dispatch(push("/"))}></button>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
